1a8555c05a084139a13d35ecdc4905259c4cf9e9
Your Name <you@example.com>
2024-06-29 10:01:06 +0000

    first commit

---
+++ src/Form/Type/ChangePasswordFormType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * Class ChangePasswordFormType.
+ *
+ * Form type for changing user password.
+ */
+class ChangePasswordFormType extends AbstractType
+{
+    /**
+     * Constructor.
+     */
+    public function __construct(private readonly TranslatorInterface $translator)
+    {
+    }
+
+    /**
+     * Builds the form.
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('currentPassword', PasswordType::class, [
+                'label' => 'Current Password',
+                'required' => true,
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => $this->translator->trans('validators.current_password'),
+                    ]),
+                ],
+            ])
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'New Password'],
+                'second_options' => ['label' => 'Repeat New Password'],
+                'invalid_message' => $this->translator->trans('validators.password_mismatch'),
+                'required' => true,
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => $this->translator->trans('validators.new_password'),
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => $this->translator->trans('validators.password_length', ['{{ limit }}' => 6]),
+                        'max' => 4096,
+                    ]),
+                    new NotCompromisedPassword(),
+                ],
+            ]);
+    }
+
+    /**
+     * Configures the form options.
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
